d0d68346a07c7f056526556ea57a3be6a3a943ed
xd00099 <xd00099@berkeley.edu>
2022-12-02 01:45:35 -0500

    improve formatting based on pylint

--- service/routes.py
+++ service/routes.py
@@ -61,7 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -80,7 +80,7 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -100,6 +100,7 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -111,15 +112,14 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
     account.deserialize(request.get_json())
     account.update()
-    
     return account.serialize(), status.HTTP_200_OK
-    
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
@@ -132,7 +132,6 @@ def delete_accounts(account_id):
     account = Account.find(account_id)
     if account:
         account.delete()
-        
     return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
