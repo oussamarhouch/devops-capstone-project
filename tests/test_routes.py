75b41eeb78ec43d03a48a866cbee2826b3f527c1
xd00099 <xd00099@berkeley.edu>
2022-12-01 23:57:58 -0500

    done implmenting delete

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -170,3 +170,13 @@ class TestAccountService(TestCase):
         """It should not update an Account that is not found"""
         resp = self.client.put(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+
+        response = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    
+    
+    
